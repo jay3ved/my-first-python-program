d0c7b2662358c8ffe21700bf51beb0b8f35bb77b
jay3ved <jay3ved@gmail.com>
2026-07-24 18:04:46 +0530

    Add files via upload
    First Python upload

---
+++ my_program.py
@@ -0,0 +1,67 @@
+#name = input("Jay")
+#print("Hello " + name)
+#print("Python program run successfully")
+
+#if else age program
+#age = int(input("Enter your Age :"))
+#if age >= 18:
+#    print("you are eigigble to give your vote.")
+#else:
+#    print("you are too small to give your vote.")
+
+#if else Marks program
+#marks = int(input("Enter your marks :"))
+#if marks >=35:
+#    print("Congratulations! You are Pass.")
+#else:
+#    print("Sorry! You are fail.")
+
+#if elseif else program
+#light = input("Enter light colour (Red/Yellow/Green) :").lower()
+#if light == "red":
+#    print("Stop your car")
+#elif light == "yellow":
+#    print("Wait")
+#else:
+#    print("Go")
+
+# Season program
+#month = input("Enter month (March/June/November etc..)").lower()
+#if month == "march" or month =="april" or month =="may":
+#    print("Summer Season")
+#elif month == month == "june" or month =="july" or month =="augast":
+#    print("Monsoon Season")
+#else:
+#    print("Winter Season")
+
+# Loops
+#for i in range(1,51):
+#    print(i)
+
+#Table
+
+#num = 17
+
+#for i in range(1,11):
+#    print(num, "X", i, "=",  num * i)
+
+#list
+
+##vegetable= ["Tomato", "Potato", "Brinjal", "Cauliflower"]
+##print ("First Name :", vegetable[0])
+##print ("First Name :", vegetable[2])
+##vegetable.append("Sweet Potato")
+##print("After added new one :" , vegetable)
+##vegetable.remove("Brinjal")
+##print("After removed one :" , vegetable)
+##print("Vegetabe list")
+##for vegetables in vegetable:
+##    print("Vegetabe Name :", vegetables)
+
+##Function programm
+def welcome_user(username):
+    print("Hello " + username + "!")
+    print ("Welcome")
+
+welcome_user("Jay")
+ 
